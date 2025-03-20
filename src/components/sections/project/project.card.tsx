c27d8eadef0e0cb7862bc66908dd541c4cdee13f
Phan Anh Mỹ <105805778+taikhoanpthan@users.noreply.github.com>
2025-03-20 22:40:07 +0700

    @@

--- src/components/sections/project/project.card.tsx
+++ src/components/sections/project/project.card.tsx
@@ -1,48 +1,88 @@
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 interface IProps {
-    imgPath: string;
-    title: string;
-    description: string;
-    githubLink: string;
-    demoLink: string;
+  imgPath: string;
+  title: string;
+  description: string;
+  githubLink: string;
+  demoLink: string;
 }
 
 function ProjectCard(props: IProps) {
-    return (
-        <Card className="project-card-view">
-            <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ maxHeight: 215, objectFit: "cover" }} />
-            <Card.Body className="d-flex flex-column">
-                <Card.Title>
-                    {props.title}
-                </Card.Title>
-                <div className="d-flex flex-column justify-content-between h-100">
-                    <Card.Text style={{ textAlign: "justify" }}>
-                        {props.description}
-                    </Card.Text>
-                    <div>
-                        <Button variant="primary" href={props.githubLink} target="_blank">
-                            <BsGithub /> &nbsp;
-                            GitHub
-                        </Button>
+  const [showFullDesc, setShowFullDesc] = useState(false);
+  const maxLength = 200; // Số ký tự hiển thị ban đầu
 
-                        <Button
-                            variant="primary"
-                            href={props.demoLink}
-                            target="_blank"
-                            style={{ marginLeft: "10px" }}
-                        >
-                            <CgWebsite /> &nbsp;
-                            {"Demo"}
-                        </Button>
+  const handleToggleDescription = () => {
+    setShowFullDesc(!showFullDesc);
+  };
 
-                    </div>
-                </div>
-            </Card.Body>
-        </Card>
-    );
+  const renderDescription = () => {
+    if (props.description.length <= maxLength) {
+      return props.description;
+    }
+
+    if (showFullDesc) {
+      return (
+        <>
+          {props.description}{" "}
+          <span
+            style={{ color: "blue", cursor: "pointer" }}
+            onClick={handleToggleDescription}
+          >
+            Ẩn bớt
+          </span>
+        </>
+      );
+    } else {
+      return (
+        <>
+          {props.description.substring(0, maxLength)}...{" "}
+          <span
+            style={{ color: "blue", cursor: "pointer" }}
+            onClick={handleToggleDescription}
+          >
+            Xem thêm
+          </span>
+        </>
+      );
+    }
+  };
+
+  return (
+    <Card className="project-card-view">
+      <Card.Img
+        variant="top"
+        src={props.imgPath}
+        alt="card-img"
+        style={{ maxHeight: 215, objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title>{props.title}</Card.Title>
+        <div className="d-flex flex-column justify-content-between h-100">
+          <Card.Text style={{ textAlign: "justify" }}>
+            {renderDescription()}
+          </Card.Text>
+          <div>
+            <Button variant="primary" href={props.githubLink} target="_blank">
+              <BsGithub /> &nbsp;GitHub
+            </Button>
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;Demo
+            </Button>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
+
 export default ProjectCard;
--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -52,17 +52,17 @@ export const PROJECTS = [
     githubLink: "https://github.com/taikhoanpthan/KLTN",
     demoLink: "https://www.anhmybakery.shop/",
   },
-  // {
-  //   id: 2,
-  //   imgPath: springRestImg,
-  //   title: "Java Spring REST",
-  //   description:
-  //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  //   githubLink:
-  //     "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
-  //   demoLink:
-  //     "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
-  // },
+  {
+    id: 2,
+    imgPath: springRestImg,
+    title: "Java Spring REST",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    githubLink:
+      "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
+    demoLink:
+      "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
+  },
 ];
 
 export const SKILLS_DATA = [
